f343aa0d532a5e094b56d092a9a37ab5ad7fb230
giovannivicentin <giovannifvicentin@gmail.com>
2024-06-25 15:21:22 -0300

    feat: make header component responsive and add hamburger menu icon

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,40 +2,54 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { ModeToggle } from './mode-toggle'
 import { Separator } from './ui/separator'
+import { HamburgerMenuIcon } from '@radix-ui/react-icons'
 
 export function Header() {
   return (
     <div>
-      <header className="flex items-center justify-around w-full z-10 py-1">
+      <header className="flex items-center justify-between px-10 md:px-0  md:justify-around w-full z-10 py-2">
         <Link href="/">
           <Image
             alt="Image of logo in header"
-            width="320"
-            height="240"
-            className="w-24 h-18"
-            src="/logo/logo-header.png"
+            width="1600"
+            height="1200"
+            className="w-28 h-auto"
+            src="/logo/header-logo.svg"
+            priority
           ></Image>
         </Link>
 
-        <ul className="flex gap-10">
+        <ul className="hidden md:flex gap-8">
           <Link href="/">
-            <li>Início</li>
+            <li className="border-y border-transparent hover:border-b-primary hover:text-primary text-lg 2xl:">
+              Início
+            </li>
           </Link>
           <Link href="/servicos">
-            <li>Serviços </li>
+            <li className="border-y border-transparent hover:border-b-primary hover:text-primary text-lg">
+              Serviços
+            </li>
           </Link>
           <Link href="/chamados">
-            <li>Chamados</li>
+            <li className="border-y border-transparent hover:border-b-primary hover:text-primary text-lg">
+              Chamados
+            </li>
           </Link>
           <Link href="/portfolio">
-            <li>Portfólio</li>
+            <li className="border-y border-transparent hover:border-b-primary hover:text-primary text-lg">
+              Portfólio
+            </li>
           </Link>
           <Link href="/uteis">
-            <li>Uteis</li>
+            <li className="border-y border-transparent hover:border-b-primary hover:text-primary text-lg">
+              Uteis
+            </li>
           </Link>
         </ul>
-
-        <ModeToggle />
+        <div className="flex justify-center items-center gap-4">
+          <ModeToggle />
+          <HamburgerMenuIcon className="md:hidden h-[1.2rem] w-[1.2rem]" />
+        </div>
       </header>
       <Separator />
     </div>
