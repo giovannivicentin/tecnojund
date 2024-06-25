0b1ded0ba36788f16f9dd89b0303ac0c146d058f
giovannivicentin <giovannifvicentin@gmail.com>
2024-06-25 17:36:10 -0300

    feat: Make header component responsive with hamburger menu icon

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,27 +1,37 @@
+'use client'
 import Link from 'next/link'
 import Image from 'next/image'
+import { useState, useCallback } from 'react'
 import { ModeToggle } from './mode-toggle'
 import { Separator } from './ui/separator'
-import { HamburgerMenuIcon } from '@radix-ui/react-icons'
+import { HamburgerMenuIcon, Cross2Icon } from '@radix-ui/react-icons'
+import { Button } from './ui/button'
 
 export function Header() {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const handleNav = useCallback(() => {
+    setMenuOpen((prev) => !prev)
+  }, [])
+
   return (
     <div>
-      <header className="flex items-center justify-between px-10 md:px-0  md:justify-around w-full z-10 py-2">
+      <header className="flex items-center justify-between px-4 md:px-0 md:justify-around w-full z-10 py-2">
         <Link href="/">
           <Image
-            alt="Image of logo in header"
+            alt="Logo"
             width="1600"
             height="1200"
             className="w-28 h-auto"
             src="/logo/header-logo.svg"
             priority
-          ></Image>
+          />
         </Link>
 
+        {/* Menu desktop sempre visível na largura md para cima */}
         <ul className="hidden md:flex gap-8">
           <Link href="/">
-            <li className="border-y border-transparent hover:border-b-primary hover:text-primary text-lg 2xl:">
+            <li className="border-y border-transparent hover:border-b-primary hover:text-primary text-lg">
               Início
             </li>
           </Link>
@@ -42,13 +52,67 @@ export function Header() {
           </Link>
           <Link href="/uteis">
             <li className="border-y border-transparent hover:border-b-primary hover:text-primary text-lg">
-              Uteis
+              Úteis
             </li>
           </Link>
         </ul>
-        <div className="flex justify-center items-center gap-4">
+
+        <div className="flex justify-center items-center">
           <ModeToggle />
-          <HamburgerMenuIcon className="md:hidden h-[1.2rem] w-[1.2rem]" />
+          <Button
+            variant="ghost"
+            onClick={handleNav}
+            className="hover:text-primary cursor-pointer md:hidden"
+            aria-controls="mobile-menu"
+            aria-expanded={menuOpen}
+          >
+            <HamburgerMenuIcon className="h-[1.2rem] w-[1.2rem]" />
+          </Button>
+        </div>
+
+        {/* Menu mobile */}
+        <div
+          id="mobile-menu"
+          className={`fixed left-0 top-0 w-full md:hidden h-2/5 bg-background-color/70 backdrop-blur-lg px-4 pt-4 transform ${
+            menuOpen ? 'translate-y-0' : '-translate-y-full'
+          } transition-transform duration-100 ease-in-out`}
+        >
+          <div className="flex w-full items-center justify-end">
+            <Button
+              variant="ghost"
+              onClick={handleNav}
+              className="cursor-pointer hover:text-primary"
+            >
+              <Cross2Icon className="h-[1.4rem] w-[1.4rem]" />
+            </Button>
+          </div>
+          <ul className="flex flex-col text-center  gap-4 mt-4">
+            <Link href="/">
+              <li className="text-lg hover:text-primary" onClick={handleNav}>
+                Início
+              </li>
+            </Link>
+            <Link href="/servicos">
+              <li className="text-lg hover:text-primary" onClick={handleNav}>
+                Serviços
+              </li>
+            </Link>
+            <Link href="/chamados">
+              <li className="text-lg hover:text-primary" onClick={handleNav}>
+                Chamados
+              </li>
+            </Link>
+            <Link href="/portfolio">
+              <li className="text-lg hover:text-primary" onClick={handleNav}>
+                Portfólio
+              </li>
+            </Link>
+            <Link href="/uteis">
+              <li className="text-lg hover:text-primary" onClick={handleNav}>
+                Úteis
+              </li>
+            </Link>
+          </ul>
         </div>
       </header>
       <Separator />
