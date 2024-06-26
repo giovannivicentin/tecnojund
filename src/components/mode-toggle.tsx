18256803e9e1da820b509db1c81aa80634b866fe
giovannivicentin <giovannifvicentin@gmail.com>
2024-06-26 11:59:36 -0300

    feat: Add toggle component for theme mode

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -1,39 +1,49 @@
 'use client'
 
-import { MoonIcon, SunIcon } from '@radix-ui/react-icons'
+import { useState, useEffect } from 'react'
+import { Moon, Sun, Loader } from 'lucide-react'
 import { useTheme } from 'next-themes'
 
-import { Button } from '@/components/ui/button'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
+import { Toggle } from '@/components/ui/toggle'
 
 export function ModeToggle() {
-  const { setTheme } = useTheme()
+  const { theme, resolvedTheme, setTheme } = useTheme()
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  const handleToggle = () => {
+    if (resolvedTheme === 'dark') {
+      setTheme('light')
+    } else {
+      setTheme('dark')
+    }
+  }
+
+  const displayIcon = () => {
+    if (!mounted) {
+      return <Loader className="animate-spin h-[1.2rem] w-[1.2rem]" />
+    }
+
+    if (theme === 'system') {
+      return resolvedTheme === 'dark' ? (
+        <Sun className="h-[1.2rem] w-[1.2rem] hover:text-primary" />
+      ) : (
+        <Moon className="h-[1.2rem] w-[1.2rem] hover:text-primary" />
+      )
+    }
+    return theme === 'dark' ? (
+      <Sun className="h-[1.2rem] w-[1.2rem] hover:text-primary" />
+    ) : (
+      <Moon className="h-[1.2rem] w-[1.2rem] hover:text-primary" />
+    )
+  }
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="icon">
-          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme('light')}>
-          Claro
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('dark')}>
-          Escuro
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('system')}>
-          Sistema
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Toggle variant="default" aria-label="Toggle theme" onClick={handleToggle}>
+      {displayIcon()}
+    </Toggle>
   )
 }
