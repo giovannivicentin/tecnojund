9060d91a48e215c76b0e3404ecabd56b37370ff4
giovannivicentin <giovannifvicentin@gmail.com>
2024-07-03 14:49:26 -0300

    feat: Update carousel titles and descriptions for business and residential sections

--- src/components/carousel-condominiums.tsx
+++ src/components/carousel-condominiums.tsx
@@ -27,8 +27,8 @@ export function CarouselCondominiums() {
         <CarouselItem className="md:basis-1/2 lg:basis-1/3">
           <div className="p-1">
             <CarouselCard
-              title="awdad"
-              description="aaa"
+              title="Portarias Autônomas"
+              description="Implementamos portarias autônomas com controle de acesso e processos personalizados, integráveis com portaria virtual se necessário."
               src="/header-logo.svg"
               alt="adwa"
             />
@@ -37,8 +37,8 @@ export function CarouselCondominiums() {
         <CarouselItem className="md:basis-1/2 lg:basis-1/3">
           <div className="p-1">
             <CarouselCard
-              title="awdad"
-              description="aaa"
+              title="Controle de Acesso Completo"
+              description="Gerenciamos o acesso de funcionários, prestadores e visitantes, garantindo segurança e eficiência."
               src="/header-logo.svg"
               alt="adwa"
             />
@@ -47,8 +47,8 @@ export function CarouselCondominiums() {
         <CarouselItem className="md:basis-1/2 lg:basis-1/3">
           <div className="p-1">
             <CarouselCard
-              title="awdad"
-              description="aaa"
+              title="Automação de Condomínios"
+              description="Automatizamos processos em seu condomínio com CLP, Arduino ou Raspberry, economizando mão de obra e melhorando a eficiência."
               src="/header-logo.svg"
               alt="adwa"
             />
@@ -57,19 +57,28 @@ export function CarouselCondominiums() {
         <CarouselItem className="md:basis-1/2 lg:basis-1/3">
           <div className="p-1">
             <CarouselCard
-              title="awdad"
-              description="aaa"
+              title="Telemetria Essencial"
+              description="Antes exclusivo para grandes empresas, a telemetria agora é indispensável. Monitore em tempo real e evite desperdícios com um simples vazamento de água."
               src="/header-logo.svg"
               alt="adwa"
             />
           </div>
         </CarouselItem>
-
         <CarouselItem className="md:basis-1/2 lg:basis-1/3">
           <div className="p-1">
             <CarouselCard
-              title="awdad"
-              description="aaa"
+              title="Suporte para Portarias 24h"
+              description="Oferecemos suporte contínuo para controladores de acesso, aumentando a segurança e melhorando a experiência no condomínio. Verifique seus relatórios de acesso e veja como podemos ajudar."
+              src="/header-logo.svg"
+              alt="adwa"
+            />
+          </div>
+        </CarouselItem>
+        <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+          <div className="p-1">
+            <CarouselCard
+              title="Serviços de Manutenção"
+              description="Oferecemos reparos, vendas, integrações e contratos de manutenção mensal. Estamos prontos para ajudar no que você precisar."
               src="/header-logo.svg"
               alt="adwa"
             />
