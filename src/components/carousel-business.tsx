0800c28fb795e8de439b8eb496977abc7d42dd3a
giovannivicentin <giovannifvicentin@gmail.com>
2024-07-03 17:16:55 -0300

    feat: update business carousel images and alt text for business section

--- src/components/carousel-business.tsx
+++ src/components/carousel-business.tsx
@@ -27,20 +27,10 @@ export function CarouselBusiness() {
         <CarouselItem className="md:basis-1/2 lg:basis-1/3">
           <div className="p-1">
             <CarouselCard
-              title="Sistemas de CFTV"
-              description="Instalamos CFTV com excelente custo-benefício, reduzindo custos e aumentando a segurança em rondas e fiscalizações."
-              src="/header-logo.svg"
-              alt="adwa"
-            />
-          </div>
-        </CarouselItem>
-        <CarouselItem className="md:basis-1/2 lg:basis-1/3">
-          <div className="p-1">
-            <CarouselCard
               title="Controle de Acesso"
               description="Oferecemos controle de acesso para funcionários e prestadores com a qualidade e cuidado que sua empresa merece."
-              src="/header-logo.svg"
-              alt="adwa"
+              src="/business/ticket-gate.jpg"
+              alt="An image of a ticket gate. The image is from www.freepik.com"
             />
           </div>
         </CarouselItem>
@@ -49,8 +39,8 @@ export function CarouselBusiness() {
             <CarouselCard
               title="Automação Personalizada"
               description="Desenvolvemos soluções personalizadas de automação de processos e controles com CLP, Arduino ou Raspberry."
-              src="/header-logo.svg"
-              alt="adwa"
+              src="/business/automation.jpg"
+              alt="Young inventor creating in her workshop. The image is from www.freepik.com"
             />
           </div>
         </CarouselItem>
@@ -59,19 +49,28 @@ export function CarouselBusiness() {
             <CarouselCard
               title="Gerenciamento de Redes e Servidores"
               description="Substitua vários servidores físicos por um único com segurança e economia. Gerenciamos VPN, arquivos, domínio, backup, ERP e SQL."
-              src="/header-logo.svg"
-              alt="adwa"
+              src="/business/server.jpg"
+              alt="Technician checking the server hub. The image is from www.freepik.com"
             />
           </div>
         </CarouselItem>
-
+        <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+          <div className="p-1">
+            <CarouselCard
+              title="Sistemas de CFTV"
+              description="Instalamos CFTV com excelente custo-benefício, reduzindo custos e aumentando a segurança em rondas e fiscalizações."
+              src="/business/camera.jpg"
+              alt="CCTV security camera on the ceiling. Image from www.freepik.com"
+            />
+          </div>
+        </CarouselItem>
         <CarouselItem className="md:basis-1/2 lg:basis-1/3">
           <div className="p-1">
             <CarouselCard
               title="Rede Estruturada"
               description="Organizamos sua rede e racks para melhorar desempenho e economia."
-              src="/header-logo.svg"
-              alt="adwa"
+              src="/business/cables.jpg"
+              alt="A lot of cables together. The image is from www.freepik.com"
             />
           </div>
         </CarouselItem>
