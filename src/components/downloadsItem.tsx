d85397fc832a0ca756005daba5023588e045eb3a
giovannivicentin <giovannifvicentin@gmail.com>
2024-07-06 12:46:48 -0300

    feat: Update DownloadsItem component to use Next.js Image component

--- src/components/downloadsItem.tsx
+++ src/components/downloadsItem.tsx
@@ -1,5 +1,33 @@
-import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 
-export function DownloadsItem() {
-  return <div></div>
+type DownloadsItemProps = {
+  downloadDetails: {
+    href: string
+    name: string
+    imageSrc: string
+    imageAlt: string
+  }
+}
+
+export function DownloadsItem({ downloadDetails }: DownloadsItemProps) {
+  return (
+    <Link href={downloadDetails.href}>
+      <div className="w-full bg-primary flex flex-col justify-center md:h-80 items-center gap-2 p-2 rounded-2xl cursor-pointer">
+        <Image
+          src={downloadDetails.imageSrc}
+          alt={downloadDetails.imageAlt}
+          width={200}
+          height={200}
+          style={{
+            maxWidth: '100%',
+            height: 'auto',
+          }}
+        />
+        <h2 className="text-xl font-medium text-white md:text-3xl text-center">
+          {downloadDetails.name}
+        </h2>
+      </div>
+    </Link>
+  )
 }
