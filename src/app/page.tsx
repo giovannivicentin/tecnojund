3010e90465a587b03cbfebed85025b72f20f0aae
giovannivicentin <giovannifvicentin@gmail.com>
2024-07-06 21:42:01 -0300

    refactor: Update page layout and component structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
         'min-h-dvh bg-background flex flex-col items-center justify-center gap-12 animate-fade-up animate-once')
       }
     >
-      <div className="w-3/4 md:h-dvh mt-24 md:mt-0 flex flex-col md:flex-row items-center justify-center gap-12 md:gap-0">
+      <div className="w-3/4 md:h-[90dvh] mt-24 md:mt-0 flex flex-col md:flex-row items-center justify-center gap-12 md:gap-0">
         <div className="md:w-1/2 text-center md:text-start md:-mt-20">
           <h1 className="text-6xl font-medium">Automação</h1>
           <h2 className="text-3xl">no tempo certo</h2>
