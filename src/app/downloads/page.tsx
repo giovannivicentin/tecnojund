6abcbe4bf60dc6b6ea171d91a14e9ceb7e5fbcb7
giovannivicentin <giovannifvicentin@gmail.com>
2024-07-06 21:18:00 -0300

    feat: Update Downloads page layout and component structure

--- src/app/downloads/page.tsx
+++ src/app/downloads/page.tsx
@@ -1,16 +1,60 @@
+import { DownloadsItem } from '@/components/downloadsItem'
+
 export default function Downloads() {
   return (
-    <div className="bg-background h-dvh flex flex-col mt-12 gap-12 items-center">
-      <h1 className="font-medium text-4xl">Downloads</h1>
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-        <div className="bg-gray-200 p-4">Item 1</div>
-        <div className="bg-gray-200 p-4">Item 2</div>
-        <div className="bg-gray-200 p-4">Item 3</div>
-        <div className="bg-gray-200 p-4">Item 4</div>
-        <div className="bg-gray-200 p-4">Item 5</div>
-        <div className="bg-gray-200 p-4">Item 6</div>
-        <div className="bg-gray-200 p-4">Item 7</div>
-        <div className="bg-gray-200 p-4">Item 8</div>
+    <div className="bg-background h-dvh flex justify-center items-start">
+      <div className="flex flex-col justify-center items-center gap-12 mt-12 w-full px-4">
+        <h1 className="font-medium text-4xl text-center">Downloads</h1>
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 w-full max-w-lg md:max-w-5xl mb-24">
+          <DownloadsItem
+            href="/link-to-download"
+            name="Downloadable Content"
+            imageSrc="/home-house.png"
+            imageAlt="Descriptive Alt Text"
+          />
+          <DownloadsItem
+            href="/link-to-download"
+            name="Downloadable Content"
+            imageSrc="/home-house.png"
+            imageAlt="Descriptive Alt Text"
+          />
+          <DownloadsItem
+            href="/link-to-download"
+            name="Downloadable Content"
+            imageSrc="/home-house.png"
+            imageAlt="Descriptive Alt Text"
+          />
+          <DownloadsItem
+            href="/link-to-download"
+            name="Downloadable Content"
+            imageSrc="/home-house.png"
+            imageAlt="Descriptive Alt Text"
+          />
+          <DownloadsItem
+            href="/link-to-download"
+            name="Downloadable Content"
+            imageSrc="/home-house.png"
+            imageAlt="Descriptive Alt Text"
+          />
+          <DownloadsItem
+            href="/link-to-download"
+            name="Downloadable Content"
+            imageSrc="/home-house.png"
+            imageAlt="Descriptive Alt Text"
+          />
+          <DownloadsItem
+            href="/link-to-download"
+            name="Downloadable Content"
+            imageSrc="/home-house.png"
+            imageAlt="Descriptive Alt Text"
+          />
+          <DownloadsItem
+            href="/link-to-download"
+            name="Downloadable Content"
+            imageSrc="/home-house.png"
+            imageAlt="Descriptive Alt Text"
+          />
+        </div>
       </div>
     </div>
   )
