6886c3daa1b19b949976b1515129fe11828af107
giovannivicentin <giovannifvicentin@gmail.com>
2024-07-11 19:48:52 -0300

    style: Improve image loading and hover effects on DownloadsItem component

--- src/components/downloadsItem.tsx
+++ src/components/downloadsItem.tsx
@@ -19,20 +19,27 @@ export function DownloadsItem({
   return (
     <Link href={href}>
       <div className="group w-full border-2 dark:border flex flex-col hover:text-primary hover:dark:text-customPrimary hover:underline text-black dark:text-white justify-center md:h-48 items-center gap-2 p-2 rounded-2xl cursor-pointer hover:bg-gradient-to-r dark:hover:from-primary-foreground hover:from-background hover:to-slate-50 dark:hover:to-background hover:scale-105 hover:shadow-2xl transition-all duration-300">
-        <div className="relative w-full flex justify-center items-center h-32">
+        <div className="relative w-full flex justify-center items-center h-32 transition-opacity duration-500">
           <Image
             src={imageSrc}
             alt={imageAlt}
-            layout="fill"
-            objectFit="contain"
-            className="dark:invert block group-hover:hidden"
+            className="dark:invert block absolute opacity-100 group-hover:opacity-0 transition-opacity duration-500"
+            fill
+            sizes="100vw"
+            style={{
+              objectFit: 'contain',
+            }}
           />
           <Image
             src={hoverImageSrc}
             alt={imageAlt}
-            layout="fill"
-            objectFit="contain"
-            className="hidden group-hover:block absolute"
+            className="hidden group-hover:block absolute opacity-0 group-hover:opacity-100 transition-opacity duration-500"
+            fill
+            priority
+            sizes="100vw"
+            style={{
+              objectFit: 'contain',
+            }}
           />
         </div>
         <h2 className="text-xl font-medium text-center">{name}</h2>
