75b7145b45815d70de551eab411ff3698e0cc9d5
giovannivicentin <giovannifvicentin@gmail.com>
2024-07-11 18:07:05 -0300

    style: Update download page icons and hover effects

--- src/app/downloads/page.tsx
+++ src/app/downloads/page.tsx
@@ -9,49 +9,57 @@ export default function Downloads() {
           <DownloadsItem
             href="/downloads/team-viewer.exe"
             name="Team Viewer"
-            imageSrc="/downloads-icons/team-viewer.png"
+            imageSrc="/downloads-icons/team-viewer.svg"
+            hoverImageSrc="/downloads-icons/team-viewer-hover.svg"
             imageAlt="logo of team viewer"
           />
           <DownloadsItem
             href="/downloads/netscan.zip"
             name="Netscan Digital"
-            imageSrc="/downloads-icons/netscan-digital.png"
+            imageSrc="/downloads-icons/netscan.svg"
+            hoverImageSrc="/downloads-icons/netscan-hover.svg"
             imageAlt="logo of netscan digital"
           />
           <DownloadsItem
             href="/downloads/arduino-driver.zip"
             name="Arduino Driver"
-            imageSrc="/downloads-icons/arduino.png"
+            imageSrc="/downloads-icons/arduino.svg"
+            hoverImageSrc="/downloads-icons/arduino-hover.svg"
             imageAlt="logo of arduino"
           />
           <DownloadsItem
             href="/downloads/winrar.exe"
             name="WinRAR"
-            imageSrc="/downloads-icons/winrar.png"
+            imageSrc="/downloads-icons/winrar.svg"
+            hoverImageSrc="/downloads-icons/winrar-hover.svg"
             imageAlt="logo of winrar"
           />
           <DownloadsItem
             href="/downloads/hd-test.exe"
             name="Teste de HD"
-            imageSrc="/downloads-icons/hd.png"
+            imageSrc="/downloads-icons/hd.svg"
+            hoverImageSrc="/downloads-icons/hd-hover.svg"
             imageAlt="image of hd drawing"
           />
           <DownloadsItem
             href="/downloads/folder-size.exe"
             name="Folder Size"
-            imageSrc="/downloads-icons/folder-size.png"
+            imageSrc="/downloads-icons/folder.svg"
+            hoverImageSrc="/downloads-icons/folder-hover.svg"
             imageAlt="logo of folder size"
           />
           <DownloadsItem
             href="/downloads/ln-bio.zip"
             name="Biometria LN-Bio"
-            imageSrc="/downloads-icons/ln-bio.png"
+            imageSrc="/downloads-icons/digital.svg"
+            hoverImageSrc="/downloads-icons/digital-hover.svg"
             imageAlt="logo of ln-bio"
           />
           <DownloadsItem
             href="/downloads/computer-test.exe"
             name="Teste Computador"
-            imageSrc="/downloads-icons/test-computer.png"
+            imageSrc="/downloads-icons/computer.svg"
+            hoverImageSrc="/downloads-icons/computer-hover.svg"
             imageAlt="image of computer with a test written on screen"
           />
         </div>
--- src/components/downloadsItem.tsx
+++ src/components/downloadsItem.tsx
@@ -5,6 +5,7 @@ type DownloadsItemProps = {
   href: string
   name: string
   imageSrc: string
+  hoverImageSrc: string
   imageAlt: string
 }
 
@@ -12,22 +13,28 @@ export function DownloadsItem({
   href,
   name,
   imageSrc,
+  hoverImageSrc,
   imageAlt,
 }: DownloadsItemProps) {
   return (
     <Link href={href}>
-      <div className="w-full border-2 flex flex-col hover:underline text-black hover:text-primary hover:dark:text-primary dark:text-white justify-center md:h-48 items-center gap-2 p-2 rounded-2xl cursor-pointer hover:bg-gradient-to-r dark:hover:from-primary-foreground hover:from-background hover:to-slate-50 dark:hover:to-background hover:scale-105 hover:shadow-2xl transition-all duration-300">
-        <Image
-          src={imageSrc}
-          alt={imageAlt}
-          width={100}
-          height={100}
-          style={{
-            maxWidth: '100%',
-            height: 'auto',
-          }}
-          className="dark:invert"
-        />
+      <div className="group w-full border-2 dark:border flex flex-col hover:text-primary hover:dark:text-customPrimary hover:underline text-black dark:text-white justify-center md:h-48 items-center gap-2 p-2 rounded-2xl cursor-pointer hover:bg-gradient-to-r dark:hover:from-primary-foreground hover:from-background hover:to-slate-50 dark:hover:to-background hover:scale-105 hover:shadow-2xl transition-all duration-300">
+        <div className="relative w-full flex justify-center items-center h-32">
+          <Image
+            src={imageSrc}
+            alt={imageAlt}
+            layout="fill"
+            objectFit="contain"
+            className="dark:invert block group-hover:hidden"
+          />
+          <Image
+            src={hoverImageSrc}
+            alt={imageAlt}
+            layout="fill"
+            objectFit="contain"
+            className="hidden group-hover:block absolute"
+          />
+        </div>
         <h2 className="text-xl font-medium text-center">{name}</h2>
       </div>
     </Link>
