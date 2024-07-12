2b34f71b4bbbcfd0a996e670c501a1cb4ed8aa16
giovannivicentin <giovannifvicentin@gmail.com>
2024-07-12 15:27:06 -0300

    style: Update package.json dependencies and add @radix-ui/react-dialog

--- src/app/chamados/page.tsx
+++ src/app/chamados/page.tsx
@@ -2,7 +2,7 @@ import { TicketForm } from '@/components/TicketForm'
 
 function Chamados() {
   return (
-    <div className="mt-12 bg-background flex flex-col items-center justify-center">
+    <div className="mt-12 min-h-dvh bg-background flex flex-col items-center">
       <h1 className="font-medium text-4xl text-center mb-6">Chamados</h1>
 
       <TicketForm />
--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,5 +1,7 @@
 function Portfolio() {
-  return <div>Portfolio</div>
+  return (
+    <div className="min-h-dvh flex items-center justify-center">Portfolio</div>
+  )
 }
 
 export default Portfolio
