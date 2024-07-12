a01d1943857d6c795389051168e1420f5eb131c8
giovannivicentin <giovannifvicentin@gmail.com>
2024-07-12 09:47:55 -0300

    style: Update "Chamados" page title styling and margin

--- src/app/chamados/page.tsx
+++ src/app/chamados/page.tsx
@@ -3,7 +3,7 @@ import { TicketForm } from '@/components/TicketForm'
 function Chamados() {
   return (
     <div className="mt-12 bg-background flex flex-col items-center justify-center">
-      <h1 className="font-medium text-4xl text-center">Chamados</h1>
+      <h1 className="font-medium text-4xl text-center mb-6">Chamados</h1>
 
       <TicketForm />
     </div>
