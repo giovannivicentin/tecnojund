2d1455c5973a3c63ab2d5737f778388885dc1c82
giovannivicentin <giovannifvicentin@gmail.com>
2024-07-16 09:25:53 -0300

    style: Update Footer component layout and image loading

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -8,41 +8,41 @@ export function Footer() {
     <>
       <Separator />
       <footer className="w-full bg-background dark:bg-background pt-4">
-        <div className="flex flex-col-reverse md:flex-row justify-evenly gap-8">
-          <div className="flex flex-col-reverse md:flex-col justify-center text-center items-center">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 lg:ml-32">
+          <div className="flex flex-col justify-center items-center">
             <Image
               src="/header-logo.svg"
               alt="Logo Tecnojund"
               width={180}
               height={90}
-            ></Image>
-            <div className="flex gap-2 items-center justify-center text-transparent">
-              <EnvelopeClosedIcon className="w-4 h-4 hidden" />
-              contato@tecnojund.com.br
-            </div>
+            />
           </div>
-          <div className="flex flex-col text-center gap-2">
-            <h3 className="text-xl font-bold my-2 text-center">Fale Conosco</h3>
-            <div className="flex gap-2 items-center justify-center">
-              <EnvelopeClosedIcon className="w-4 h-4" />
+          <div className="flex flex-col justify-center items-center md:items-start">
+            <h3 className="text-xl font-bold my-2 text-center md:text-start">
+              Horário de Funcionamento
+            </h3>
+            <p className="text-md text-center md:text-start">
+              Segunda a sexta <br /> 8h às 18h
+            </p>
+          </div>
+          <div className="flex flex-col gap-2 justify-center items-center md:items-start">
+            <h3 className="text-xl font-bold my-2 text-center md:text-start">
+              Fale Conosco
+            </h3>
+            <div className="">
               <Link
                 href="mailto:contato@tecnojund.com.br"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="hover:underline hover:text-primary"
+                className="flex gap-2 items-center justify-center md:justify-start hover:underline hover:text-primary"
               >
+                <EnvelopeClosedIcon className="w-4 h-4" />
                 contato@tecnojund.com.br
               </Link>
             </div>
           </div>
-          <div className="flex flex-col text-center md:text-end">
-            <h3 className="text-xl font-bold my-2">Horário de Funcionamento</h3>
-            <p className="text-md">
-              Segunda a sexta <br /> 8h às 18h
-            </p>
-          </div>
         </div>
-        <div className="w-full my-4 text-sm text-center flex flex-col items-center justify-center text-muted-foreground">
+        <div className="w-full my-4 text-sm flex flex-col items-center justify-center text-muted-foreground">
           <p>© Tecnojund 2024. Todos os direitos reservados.</p>
           <Link
             className="hover:underline hover:text-primary"
